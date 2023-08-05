2ca7f399c69fb047cf3bcb8da1756e412ebfe02e
noe <noe.jaraq@gmail.com>
2023-08-05 15:21:33 -0500

    html's to ejs's

--- index.js
+++ index.js
@@ -4,15 +4,38 @@ import express from "express";
 const app = express();
 const port = 8080;
 
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
+let tasks = [
+    {
+        title: "Task title",
+        category: "Today"
+    }
+];
+
+let categories = [
+    "Today",
+    "Work"
+]
+
+app.get("/", (req, res)=>{
+    res.redirect("/tasks");
+})
 
 app.get("/tasks", (req, res)=>{
-
+    res.render("index.ejs", {
+        tasks: tasks,
+        categories: categories
+    });
 })
 
 app.post("/newTask", (req, res)=>{
-
+    tasks.push({
+        title: req.body.task,
+        category: req.body.category
+    })
+    res.redirect("/");
 })
 
 app.listen(port, ()=>{
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,10 +2,12 @@ const modal = document.querySelector("#newTaskModal");
 const closeModalBtn = document.querySelector("#closeModalBtn");
 const overlay = document.querySelector("#modalOverlay");
 const newTaskBtn = document.querySelector("#newTaskBtn");
+const taskInput = document.querySelector("input[name='task']");
 
 const closeModal = function(){
     modal.classList.add("hidden");
     overlay.classList.add("hidden");
+    taskInput.value = "";
 }
 
 const openModal = function(){
@@ -20,4 +22,9 @@ document.addEventListener("keydown", (e)=>{
     if(e.key === "Escape" && !modal.classList.contains("hidden")){
         closeModal();
     }
+})
+
+//validate form
+taskInput.addEventListener("input", (e)=>{
+    console.log(this.value);
 })
