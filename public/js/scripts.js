d7f751f0602c08298098bc1d5fee73b58f4ece5c
noe <noe.jaraq@gmail.com>
2023-08-06 16:45:00 -0500

    new category btn added

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,36 +1,56 @@
-const modal = document.querySelector("#newTaskModal");
-const closeModalBtn = document.querySelector("#closeModalBtn");
+const modalTask = document.querySelector("#newTaskModal");
+const modalCategory = document.querySelector("#newCategoryModal");
+const closeModalTaskBtn = document.querySelector("#closeModalTaskBtn");
+const closeModalCategoryBtn = document.querySelector("#closeModalCategoryBtn");
 const overlay = document.querySelector("#modalOverlay");
+
 const newTaskBtn = document.querySelector("#newTaskBtn");
+const newCategoryBtn = document.querySelector("#newCategoryBtn");
+
 const taskInput = document.querySelector("input[name='task']");
-const addTaskBtn = document.querySelector("#addTaskBtn");
 const completedBtns = document.querySelectorAll(".completedBtn");
-const tasksLeft = document.querySelector("#tasksLeft");
 
 //open and close modal functions
-const closeModal = function(){
-    modal.classList.add("hidden");
+const closeModal = function(m){
+    m.classList.add("hidden");
     overlay.classList.add("hidden");
     taskInput.value = "";
 }
 
-const openModal = function(){
-    modal.classList.remove("hidden");
+const openModal = function(m){
+    m.classList.remove("hidden");
     overlay.classList.remove("hidden");
 }
 
-//open and close events
-newTaskBtn.addEventListener("click", openModal);
-closeModalBtn.addEventListener("click", closeModal);
-overlay.addEventListener("click", closeModal);
+//open and close modal task events
+newTaskBtn.addEventListener("click", ()=>{
+    openModal(modalTask);
+});
+closeModalTaskBtn.addEventListener("click", ()=>{
+    closeModal(modalTask);
+});
+overlay.addEventListener("click", ()=>{
+    closeModal(modalTask);
+    closeModal(modalCategory);
+});
 document.addEventListener("keydown", (e)=>{
-    if(e.key === "Escape" && !modal.classList.contains("hidden")){
-        closeModal();
+    if(e.key === "Escape" && (!modalTask.classList.contains("hidden")||!modalCategory.classList.contains("hidden"))){
+        closeModal(modalTask);
+        closeModal(modalCategory);
     }
 })
 
+//open and close modal category events
+newCategoryBtn.addEventListener("click", ()=>{
+    openModal(modalCategory);
+})
+closeModalCategoryBtn.addEventListener("click", ()=>{
+    closeModal(modalCategory);
+});
+
 //validate form
 taskInput.addEventListener("keyup", (e)=>{
+    const addTaskBtn = document.querySelector("#addTaskBtn");
     if(e.target.value===""){
         addTaskBtn.disabled = true;
     }
@@ -45,7 +65,6 @@ for(let i = 0; i < completedBtns.length; i++){
         let parent = e.target.parentElement.parentElement;
         if(!parent.classList.contains("completedTask")){
             e.target.parentElement.parentElement.classList.add("completedTask");
-            console.log()
         }
     })
 }
